92839a66781965a1231df785255570166a432210
Nick <nicks@avi.com>
2024-04-07 16:26:45 -0700

    imported navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "./components/navbar";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
