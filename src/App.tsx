d01ccf681e61948f556900dd0c0c9a32c6f30100
Nick Stern <nicholas.stern2@gmail.com>
2024-04-07 18:06:38 -0700

    implemented react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from "react";
-import NavBar from "./components/navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Projects } from "./pages/Projects";
+import { Testimonials } from "./pages/Testimonials";
+import { Home } from "./pages/Home";
+import ContactForm from "./pages/Contact";
+import { Stack } from "@mui/material";
+import Layout from "./components/Layout";
+import Resume from "./pages/Resume";
+// import { Resume } from "./pages/Resume";
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <NavBar />
-    </div>
+    <Stack height={"100%"}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path='/home' element={<Home />} />
+            <Route path='/testimonials' element={<Testimonials />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/resume' element={<Resume />} />
+            <Route path='/contact' element={<ContactForm />} />
+            <Route path='/' element={<Home />} />{" "}
+            {/* This will redirect to Home for the base path */}
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </Stack>
   );
-}
-
+};
 export default App;
