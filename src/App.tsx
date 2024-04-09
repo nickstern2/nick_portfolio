89e52cc9de57ac553fff3b373a907367b39d6ac8
Nick Stern <nicholas.stern2@gmail.com>
2024-04-08 19:21:40 -0700

    Added CssBaseline and removed testimonials and added home tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Projects } from "./pages/Projects";
-import { Testimonials } from "./pages/Testimonials";
-import { Home } from "./pages/Home";
-import ContactForm from "./pages/Contact";
-import { Stack } from "@mui/material";
+import { Stack, ThemeProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import ContactForm from "./pages/Contact";
+import { Home } from "./pages/Home";
+import ProjectList from "./pages/Projects";
 import Resume from "./pages/Resume";
-import { RouteNames } from "./reusable/enum-list";
-// import { Resume } from "./pages/Resume";
+import { Testimonials } from "./pages/Testimonials";
+import { theme } from "./reusable/Theme";
+import { RouteNames } from "./reusable/consts";
 
 const App = () => {
   return (
-    <Stack height={"100%"}>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path={RouteNames.Home} element={<Home />} />
-            <Route path={RouteNames.Testimonials} element={<Testimonials />} />
-            <Route path={RouteNames.Projects} element={<Projects />} />
-            <Route path={RouteNames.Resume} element={<Resume />} />
-            <Route path={RouteNames.Contact} element={<ContactForm />} />
-            <Route path={RouteNames.Home} element={<Home />} />
-            {/* This will redirect to Home for the base path */}
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </Stack>
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Stack>
+          <BrowserRouter>
+            <Layout>
+              <Routes>
+                <Route path={RouteNames.Home} element={<Home />} />
+                <Route path={RouteNames.Projects} element={<ProjectList />} />
+                <Route path={RouteNames.Resume} element={<Resume />} />
+                <Route path={RouteNames.Contact} element={<ContactForm />} />
+                <Route path={RouteNames.Home} element={<Home />} />
+                {/* This will redirect to Home for the base path */}
+              </Routes>
+            </Layout>
+          </BrowserRouter>
+        </Stack>
+      </ThemeProvider>
+    </>
   );
 };
 export default App;
